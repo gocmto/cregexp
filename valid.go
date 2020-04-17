a4949f72631911165a1c366d5e9329f18ae3fa4f
gocmto <gocmto@gmail.com>
2020-04-17 16:51:16 +0300

    added functions: CheckEmail, CheckDomain

--- valid.go
+++ valid.go
@@ -5,6 +5,21 @@ import "regexp"
 const langRus = "rus"
 const langEng = "eng"
 
+//
+// Checking the correctness of the domain
+// Pattern: ^([a-zA-Z0-9]([a-zA-Z0-9\-]{0,61}[a-zA-Z0-9])?\.)+[a-zA-Z]{2,6}$
+func CheckDomain(someString string) bool {
+	var valid = regexp.MustCompile(`^([a-zA-Z0-9]([a-zA-Z0-9\-]{0,61}[a-zA-Z0-9])?\.)+[a-zA-Z]{2,6}$`)
+	return valid.MatchString(someString)
+}
+
+// Checking the correctness of the email address
+// Pattern: ^([a-z0-9_-]+\.)*[a-z0-9_-]+@[a-z0-9_-]+(\.[a-z0-9_-]+)*\.[a-z]{2,6}$
+func CheckEmail(someString string) bool {
+	var valid = regexp.MustCompile(`^([a-z0-9_-]+\.)*[a-z0-9_-]+@[a-z0-9_-]+(\.[a-z0-9_-]+)*\.[a-z]{2,6}$`)
+	return valid.MatchString(someString)
+}
+
 // Only numbers are allowed in a string
 // Pattern: ^[0-9]+$
 func OnlyNumbers(someString string) bool {
