1abba44fa4ee1e364bb396968a07098ea6542ec5
gocmto <gocmto@gmail.com>
2020-04-17 16:40:52 +0300

    added word "pattern"

--- valid.go
+++ valid.go
@@ -6,21 +6,21 @@ const langRus = "rus"
 const langEng = "eng"
 
 // Only numbers are allowed in a string
-// ^[0-9]+$
+// Pattern: ^[0-9]+$
 func OnlyNumbers(someString string) bool {
 	var valid = regexp.MustCompile(`^[0-9]+$`)
 	return valid.MatchString(someString)
 }
 
 // Only latin characters are allowed in a string
-// ^[a-zA-Z]+$
+// Pattern: ^[a-zA-Z]+$
 func OnlyEngCharacters(someString string) bool {
 	var valid = regexp.MustCompile(`^[a-zA-Z]+$`)
 	return valid.MatchString(someString)
 }
 
 // Only cyrillic characters allowed in the string
-// ^[а-яА-ЯёЁ]+$
+// Pattern: ^[а-яА-ЯёЁ]+$
 func OnlyRusCharacters(someString string) bool {
 	var valid = regexp.MustCompile(`^[а-яА-ЯёЁ]+$`)
 	return valid.MatchString(someString)
